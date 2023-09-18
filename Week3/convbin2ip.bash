afb1092550e1f0c5420db303126ff5bea688d1c4
root <root@Ubuntu.myguest.virtualbox.org>
2023-09-18 16:18:56 -0400

    Worked on convert binary to ip script

--- Week3/convbin2ip.bash
+++ Week3/convbin2ip.bash
@@ -1,6 +1,18 @@
 #! /bin/bash
 # convert binary to an ip address
 
+echo "Please enter a 32 digit binary number"
+
 read binary
 
+bin1=(echo binary | grep -Eo '.{8}')
+bin2=(echo binary | grep -Eo '.{8}')
+bin3=(echo binary | grep -Eo '.{8}')
+bin4=(echo binary | grep -Eo '.{8}')
 
+binary1=$(echo "obase=10;$bin1" | bc)
+binary2=$(echo "obase=10;$bin2" | bc)
+binary3=$(echo "obase=10;$bin3" | bc)
+binary4=$(echo "obase=10;$bin4" | bc)
+
+echo "${binary1}.${binary2}.${binary3}.${binary4}"
