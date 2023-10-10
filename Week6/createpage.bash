4ae343d331755391042d5f1d2bc6ffa2f205bb73
root <root@Ubuntu.myguest.virtualbox.org>
2023-10-10 18:00:29 -0400

    More work on access log report

--- Week6/createpage.bash
+++ Week6/createpage.bash
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 logFile="access.txt"
-input="access.txt"
 if [[ ! -f "${logFile}" ]]
 then
 	bash combinelog.bash
@@ -9,25 +8,36 @@ fi
 
 :> report.html
 
-echo '<html>\n<head>\n<title> Report of log files </title>\n</head>' >> report.html
-
-echo '<body>\n<table>\n<tr>\n<td>IP</td>\n<td>Time</td>\n<td>Requested Page</td>\n<td>User Agent</td>\n</tr>' >> report.html
+cat <<EOL > report.html
+<html>
+<head>
+<title>Report of log files</title>
+</head>
+<body>
+<table>
+<tr>
+<td>IP</td>
+<td>Time</td>
+<td>Requested Page</td>
+<td>User Agent</td>
+</tr>
+EOL
 
 
 while read -r line
 do
 echo '<tr>' >> report.html
-ip=cut $line -d ' ' -f 1
-times=cut $line -d ' ' -f 4 | tr -d '[' | cut -c 14
-page=cut $line -d ' ' -f 7 | tr -d '/'
-user=cut $line -d ' ' -f 12 | tr -d '"' 'curl'
+ip=$(echo "$line" | cut -d ' ' -f 1)
+times=$(echo "$line" | cut -d ' ' -f 4 | tr -d '[')
+page=$(echo "$line" | cut -d ' ' -f 7 | tr -d '/')
+user=$(echo "$line" | cut -d ' ' -f 12 | tr -d '"')
 
-echo '<td>${ip}</td>' >> report.html
-echo '<td>${times}</td>' >> report.html
-echo '<td>${page}</td>' >> report.html
-echo '<td>${user}</td>' >> report.html 
-echo '</tr>'
-done<$input
-echo '</table>\n</body>\n</html>'
+echo "<td>${ip}</td>" >> report.html
+echo "<td>${times}</td>" >> report.html
+echo "<td>${page}</td>" >> report.html
+echo "<td>${user}</td>" >> report.html 
+echo '</tr>' >> report.html
+done<"$logFile"
+echo '</table></body></html>' >> report.html
 
-cp "report.html" "~/var/www/html/"
+cp "report.html" "/var/www/html/"
