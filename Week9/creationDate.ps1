36b8d5d66f64130a04ca4e55f41375984382d10f
Michael Kerwood <michael.kerwood@mymail.champlain.edu>
2023-11-01 14:35:06 -0400

    finished week 9 work

--- Week9/creationDate.ps1
+++ Week9/creationDate.ps1
@@ -1,1 +1,2 @@
-﻿
+$files = Get-ChildItem | Where-Object {$_.CreationTime -ge "10/24/2023"} | Select Name, CreationTime
+$files | Export-Csv -Path "C:\Users\champuser\CSI230\creationDate.csv" -NoTypeInformation
---
+++ Week9/startChrome.ps1
@@ -0,0 +1,9 @@
+﻿$chrome = Get-Process | where { $_.ProcessName -eq "chrome" } | `
+select Id, ProcessName
+
+if($chrome -eq $null){
+Start-Process 'C:\Program Files\Google\Chrome\Application\chrome.exe' -ArgumentList '"https://www.champlain.edu/"'
+}
+else{
+Stop-Process -Name "chrome"
+}
