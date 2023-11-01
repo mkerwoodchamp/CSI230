9f04d8f03dd3eb50ca1a6f7bf4bc7436823246b9
Michael Kerwood <michael.kerwood@mymail.champlain.edu>
2023-11-01 14:41:20 -0400

    finished week 10 work

---
+++ Week10/lineWordCounter.ps1
@@ -0,0 +1,7 @@
+﻿cd "C:\Users\champuser\CSI230"
+
+$FilesToLookAt = Get-ChildItem -Recurse -Filter "*.bash"
+
+for ($i=0; $i -lt $FilesToLookAt.Count; $i++){
+Get-Content $FilesToLookAt[$i].FullName | Measure-Object -Line -Word
+}
